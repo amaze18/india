d654daaa856308162cf9580d5242c1d781b01519
Anupam <92312729+amaze18@users.noreply.github.com>
2023-08-06 07:29:34 -0700

    Update qa.py

--- qa.py
+++ qa.py
@@ -90,7 +90,7 @@ def crawl(url):
 
 def remove_newlines(serie):
     serie = serie.str.replace('\n', ' ')
-    sere serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
     serie = serie.str.replace('  ', ' ')
     serie = serie.str.replace('  ', ' ')
     return serie
