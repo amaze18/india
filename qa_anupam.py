aa633b1d0f9c67c064c79a9d0fc33f68e08f3ea1
Anupam <92312729+amaze18@users.noreply.github.com>
2023-08-14 01:26:37 +0530

    Update qa_anupam.py

--- qa_anupam.py
+++ qa_anupam.py
@@ -133,8 +133,7 @@ def chat_gpt(question):
     retriever = db.as_retriever(search_type='similarity', search_kwargs={"k": 5} )#do not increase k beyond 3, else
     llm = OpenAI(model='text-davinci-003',temperature=0, openai_api_key=openai.api_key)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
+  
     
     query = question
     res = qa(query)
